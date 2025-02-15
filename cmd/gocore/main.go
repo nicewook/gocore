2e075fb961287d8c9e77df0985e2b005a3516eb5
Hyunseok Jeong <hyunseok.jeong@illuminarean.com>
2025-02-15 20:47:39 +0900

    Add dependency injection setup with fx for application components

--- cmd/gocore/main.go
+++ cmd/gocore/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"go.uber.org/fx"
 
 	"github.com/nicewook/gocore/internal/config"
 	"github.com/nicewook/gocore/internal/db"
@@ -15,6 +18,35 @@ import (
 )
 
 func main() {
+
+	app := fx.New(
+		fx.Provide(
+			NewConfig,
+			NewDB,
+			echo.New,
+		),
+		fx.Provide(
+			repository.NewUserRepository,
+			repository.NewProductRepository,
+			repository.NewOrderRepository,
+		),
+		fx.Provide(
+			usecase.NewUserUseCase,
+			usecase.NewProductUseCase,
+			usecase.NewOrderUseCase,
+		),
+		fx.Invoke(
+			handler.NewUserHandler,
+			handler.NewProductHandler,
+			handler.NewOrderHandler,
+		),
+		fx.Invoke(StartServer),
+	)
+
+	app.Run()
+}
+
+func NewConfig() *config.Config {
 	env := flag.String("env", "dev", "Environment (dev, qa, stg, prod)")
 	flag.Parse()
 
@@ -29,32 +61,36 @@ func main() {
 	}
 
 	fmt.Printf("config: %+v\n", cfg)
+	return cfg
+}
 
-	// DB 연결 및 애플리케이션 로직 추가
+func NewDB(lc fx.Lifecycle, cfg *config.Config) *sql.DB {
 	dbConn, err := db.NewDBConnection(cfg)
 	if err != nil {
 		log.Fatalf("DB connection error: %v", err)
 	}
 
-	// 라우팅
-	e := echo.New()
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return dbConn.Close()
+		},
+	})
 
-	// 의존성 주입 및 핸들러 생성
-	userRepo := repository.NewUserRepository(dbConn)
-	userUseCase := usecase.NewUserUseCase(userRepo)
-	handler.NewUserHandler(e, userUseCase)
+	return dbConn
+}
 
-	productRepo := repository.NewProductRepository(dbConn)
-	productUseCase := usecase.NewProductUseCase(productRepo)
-	handler.NewProductHandler(e, productUseCase)
-
-	orderRepo := repository.NewOrderRepository(dbConn)
-	orderUseCase := usecase.NewOrderUseCase(orderRepo)
-	handler.NewOrderHandler(e, orderUseCase)
-
-	// 서버 실행
-	log.Println("Server started at :8080")
-	if err := e.Start(fmt.Sprintf(":%d", cfg.App.Port)); err != nil {
-		log.Fatal("Shutting down the server due to:", err)
-	}
+func StartServer(lc fx.Lifecycle, e *echo.Echo, cfg *config.Config) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				if err := e.Start(fmt.Sprintf(":%d", cfg.App.Port)); err != nil {
+					log.Fatal("Shutting down the server due to:", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return e.Shutdown(ctx)
+		},
+	})
 }
